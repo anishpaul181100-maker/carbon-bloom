19de16cff5ced2702d820706e37ee20967c7a1bb
= <=>
2025-11-30 22:55:16 +0530

    commit

--- carbon_bloom_code/carbon_bloom_code/settings.py
+++ carbon_bloom_code/carbon_bloom_code/settings.py
@@ -123,9 +123,6 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
-MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
